131ed6dc3aea50a87e63a3d1e740769fe150d10d
lettertea <34195263+lettertea@users.noreply.github.com>
2024-05-30 14:32:26 +0900

    Add survey site from runner example

---
+++ React-Runner-Quiz-master/src/App.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+import update from 'react-addons-update';
+import quizQuestions from './api/quizQuestions';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
+import logo from './svg/logo.svg';
+import './App.css';
+
+const initialState = {
+  counter: 0,
+  questionId: 1,
+  question: '',
+  answerOptions: [],
+  answer: '',
+  answersCount: {
+    'Sprinter': 0,
+    'Mid-distance runner': 0,
+    'Long-distance runner': 0
+  },
+  result: ''
+};
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = initialState;
+    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+    this.quizAgain = this.quizAgain.bind(this);
+  }
+
+  componentWillMount() {
+    document.title = 'Runner Type Quiz'
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions: quizQuestions[0].answers
+    });
+  }
+
+  handleAnswerSelected(event) {
+    this.setUserAnswer(event.currentTarget.value);
+
+    if (this.state.questionId < quizQuestions.length) {
+        setTimeout(() => this.setNextQuestion(), 150);
+    } else {
+        setTimeout(() => this.setResults(this.getResults()), 300);
+    }
+  }
+
+  setUserAnswer(answer) {
+    const updatedAnswersCount = update(this.state.answersCount, {
+      [answer]: {$apply: (currentValue) => currentValue + 1}
+    });
+
+    this.setState({
+        answersCount: updatedAnswersCount,
+        answer: answer
+    });
+  }
+
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+
+    this.setState({
+        counter: counter,
+        questionId: questionId,
+        question: quizQuestions[counter].question,
+        answerOptions: quizQuestions[counter].answers,
+        answer: ''
+    });
+  }
+
+  getResults() {
+    const answersCount = this.state.answersCount;
+    const answersCountKeys = Object.keys(answersCount);
+    const answersCountValues = answersCountKeys.map((key) => answersCount[key]);
+    const maxAnswerCount = Math.max.apply(null, answersCountValues);
+
+    return answersCountKeys.filter((key) => answersCount[key] === maxAnswerCount);
+  }
+
+  setResults(result) {
+    if (result.length === 1) {
+      this.setState({ result: 'You are a pure ' + result[0]});
+    } else {
+      this.setState({ result: 'You are a hybrid of a ' + result[0] + ' and a ' + result[1]});
+    }
+  }
+
+  renderQuiz() {
+    return (
+      <Quiz
+        answer={this.state.answer}
+        answerOptions={this.state.answerOptions}
+        questionId={this.state.questionId}
+        question={this.state.question}
+        questionTotal={quizQuestions.length}
+        onAnswerSelected={this.handleAnswerSelected}
+      />
+    );
+  }
+
+  quizAgain() {
+    this.setState(initialState);
+    this.componentWillMount();
+  }
+
+  renderResult() {
+    return (
+      <div>
+      <Result quizResult={this.state.result} repeatQuiz={this.quizAgain} />
+</div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>What Type of Runner Are You?</h2>
+        </div>
+        {this.state.result ? this.renderResult() : this.renderQuiz()}
+      </div>
+    );
+  }
+
+}
+
+export default App;
