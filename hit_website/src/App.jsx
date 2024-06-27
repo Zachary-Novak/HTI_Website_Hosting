0692844e00886a125420f3817cd7bf436f2cd3e1
lettertea <34195263+lettertea@users.noreply.github.com>
2024-06-27 18:36:49 +0900

    Add router

--- hit_website/src/App.jsx
+++ hit_website/src/App.jsx
@@ -6,22 +6,16 @@ import Layout from "./layout/Layout";
 
 // pages
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
 import Survey from "./pages/Survey";
 
 const App = () => {
   return (
     <Layout>
       <Container>
-
-      <Home/>
-      {/* <Container>
         <Routes>
           <Route path="/" element={<Home />} exact />
           <Route path="/survey" element={<Survey />} />
-          <Route path='*' element={<NotFound />} />
         </Routes>
-      </Container> */}
       </Container>
     </Layout>
   );
