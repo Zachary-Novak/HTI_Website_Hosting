23cccd66247c8071355511f0f1d840cab299b926
lettertea <34195263+lettertea@users.noreply.github.com>
2024-06-27 18:26:16 +0900

    Import western site with failing routes

--- hit_website/src/App.jsx
+++ hit_website/src/App.jsx
@@ -1,38 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import ButtonTest from './assets/objects.jsx'
-import './App.css'
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Layout
+import Layout from "./layout/Layout";
 
+// pages
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Survey from "./pages/Survey";
+
+const App = () => {
   return (
-    <>
-      <ButtonTest></ButtonTest>
-    {/*
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-  </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>*/}
-    </>
-  )
-}
+    <Layout>
+      <Container>
 
-export default App
+      <Home/>
+      {/* <Container>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/survey" element={<Survey />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Container> */}
+      </Container>
+    </Layout>
+  );
+};
+
+export default App;
---
+++ hit_website/src/components/Menu.jsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import { Nav, Container } from "react-bootstrap";
+import { IoNotificationsSharp } from "react-icons/io5";
+import { AiOutlineMessage } from "react-icons/ai";
+import Form from 'react-bootstrap/Form';
+import { IoIosSearch } from "react-icons/io";
+import InputGroup from 'react-bootstrap/InputGroup';
+import { FaRegCalendarDays } from "react-icons/fa6";
+import { IoIosHome } from "react-icons/io";
+const Menu = () => {
+  return (
+    <Container>
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+
+          <strong style={{fontSize:24, color:"orange"}}>
+                  <img src={"../assets/images/hti_logo_4.png"}/> 
+
+          WasedaConnect!
+          </strong>
+
+        <Nav>
+        <IoIosHome size={40} style={{marginRight:30}}/>
+        <FaRegCalendarDays size={40} style={{marginRight:30}}/>
+
+          
+        <ul style={{marginRight:200, width:300}} className="nav ">
+        <InputGroup>
+        <Form.Control disabled={true}
+                                type="text"
+                                placeholder="Search here.."
+                            />
+                                <InputGroup.Text>
+                                    <IoIosSearch size={30}/>
+                                </InputGroup.Text>
+
+                        </InputGroup>
+
+          </ul>
+
+          <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <AiOutlineMessage size={40} style={{marginRight:30}}/>
+          <IoNotificationsSharp size={40} style={{marginRight:30}}/>
+          </ul>
+
+          <div className="col-md-1 text-end">
+                LastName FirstName
+          </div>
+        </Nav>
+      </header>
+    </Container>
+  );
+};
+
+export default Menu;
